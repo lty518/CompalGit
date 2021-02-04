db1a7e3a1a54bc64bd53d0cc53de2e9484b0359b
lty518 <belty518@gmail.com>
2021-02-04 18:09:40 +0800

    add start udp server
    this part only used in keyboard mode

---
+++ Project_VRCloudGaming/udp_client_listener.py
@@ -0,0 +1,21 @@
+import subprocess
+import os
+
+global proc
+dir_path = os.path.dirname(os.path.realpath(__file__))
+def start_udp_server():
+    # global proc
+    proc = subprocess.Popen([dir_path+'/UDPReceiver'])
+    #if success:
+        #return success
+    #else:
+        #return error
+    # os.system("Project_VRCloudGaming/UDPReceiver")
+def stop_udp_server():
+    global proc
+    proc.terminate()
+    #if success:
+        #return success
+    #else:
+        #return error
+# start_udp_server()
