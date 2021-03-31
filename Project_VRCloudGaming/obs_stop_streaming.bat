5c35d4226981df7f05fe4a2bb02b1e36285f189e
lty518 <belty518@gmail.com>
2021-03-31 09:47:59 +0800

    Update obs_stop_streaming.bat
    detect if the application if running first, then shut down the application

--- Project_VRCloudGaming/obs_stop_streaming.bat
+++ Project_VRCloudGaming/obs_stop_streaming.bat
@@ -1,1 +1,11 @@
-taskkill /f /im obs64.exe
+::taskkill /f /im obs64.exe
+
+tasklist /FI "IMAGENAME eq obs64.exe" /FO CSV > search1.log 
+
+FOR /F %%A IN (search1.log) DO IF %%~zA EQU 1 GOTO end 
+
+taskkill /F /FI "IMAGENAME eq obs64.exe"
+
+:end 
+
+del search.log 
